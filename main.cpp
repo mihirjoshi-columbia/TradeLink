eb0490a02fd1e17bae63a6f73258237b02b8057d
Mihir Joshi <mnj2122@columbia.edu>
2024-12-27 11:33:00 -0800

    Started to create basic structure for just the order, curious if I can set up a TCP/IP Connection to make a server to run things off of

--- main.cpp
+++ main.cpp
@@ -1,24 +1,4 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <limit>
-#include <string>
-#include <vector>
-#include <numeric>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
-#include <memory>
-#include <variant>
-#include <optional>
-#include <tuple>
-#include <format>
+#include "req.h"
 
 enum class OrderType {
     GoodTillCancel,
@@ -48,8 +28,55 @@ class OrderbookLevelInfos {
             bids_{ bids },
             asks_{ asks }
         {}
-        const Levle
+        const LevelInfos& GetBids() const {return bids_; }
+        const LevelInfos& GetAsks() const {return asks_; }
+
+    private:
+        LevelInfos bids_;
+        LevelInfos asks_;
 };
+
+class Order {
+    public:
+        Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity):
+            orderType_ { orderType }, 
+            orderId_ { orderId }, 
+            side_ { side }, 
+            price_ { price }, 
+            initialQuantity_ { quantity }, 
+            remainingQuantity_ { quantity }, 
+        {}
+        OrderId GetOrderId() const { return orderId_; }
+        Side GetSide() const { return side_; }
+        Price GetPrice() const { return price_; }
+        OrderType GetOrderType() const { return orderType_; }
+        Quantity GetInitialQuantity() const { return initialQuantity_; }
+        Quantity GetRemainingQuantity () const { return remainingQuantity_; }
+        Quantity GetFilledQuantity () const { return GetInitialQuantity() - GetRemainingQuantity(); }
+        void Fill(Quantity quantity) {
+            if (quanity > GetRemainingQuantity()) {
+                // Throw error that size of order is too big / queue it correctly
+                throw std::logic_error(std::format("Order ({}) cannot be filled for more than its remaining quanitty.", GetOrderId()));
+            }
+            remainingQuantity_ -= quantity;
+        }
+
+    private:
+        OrderType orderType_;
+        OrderId orderId_;
+        Side side_;
+        Price price_;
+        Quantity initialQuantity_;
+        Quantity remainingQuantity_;
+};
+
+using OrderPointer = std::shared_ptr<Order>;
+using OrderPointers = std::list<OrderPointer>;
+
+class OrderModify {
+
+
+} 
 
 int main() {
     return 0;
