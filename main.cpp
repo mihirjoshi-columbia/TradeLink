41e1ff3042a7811ee708e6ae0553054c4da444e1
Mihir Joshi <mnj2122@columbia.edu>
2025-01-05 21:40:29 -0800

    started getting user input testing cmd line prompts, can maybe first demo it wiht nc

--- main.cpp
+++ main.cpp
@@ -31,8 +31,16 @@ int main() {
     int clntSock = accept(servSock, nullptr, nullptr);
     */
 
-    char* ordertype_;
-    std::cin >> ordertype_;
+    std::string cmdLineInput;
+    OrderId cmdLineId;
+    std::string cmdLineSide;
+    Price cmdLinePrice;
+    Quantity cmdLineQuantity;
+
+    std::cout << "Usage: OrderType id side price quantity";
+    std::cin >> cmdLineInput >> cmdLineId >> cmdLineSide >> cmdLinePrice >> cmdLineQuantity;
+    std::cout << cmdLineInput << cmdLineId << cmdLineSide << cmdLinePrice << cmdLineQuantity << std::endl;
+
     Orderbook orderbook;
     const OrderId orderId = 1;
     orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10));
@@ -41,5 +49,4 @@ int main() {
     std::cout << orderbook.Size() << std::endl;
     return 0;
 }
-
 
